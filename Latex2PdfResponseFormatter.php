1aae757d13092846f5056ac8d3dc040313cf0fa0
fr00ller <marco@bhima.eu>
2019-01-22 00:42:13 +0100

    First

---
+++ Latex2PdfResponseFormatter.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ *
+ * @author Marco Petrini <marco@bhima.eu>
+ * @created 15/05/14 12:35 PM
+ */
+
+namespace pcrt\latex;
+
+use Yii;
+use yii\base\Component;
+use yii\web\Response;
+use yii\web\ResponseFormatterInterface;
+
+/**
+ * Latex2PdfResponseFormatter formats the given Latex into a PDF response content.
+ *
+ * It is used by [[Response]] to format response data.
+ *
+ * @author Marco Petrini <marco@bhima.eu>
+ * @since 2.0
+ */
+class Latex2PdfResponseFormatter extends Component implements ResponseFormatterInterface
+{
+
+	public $options = [];
+
+	/**
+	 * @var Closure function($mpdf, $data){}
+	 */
+	public $beforeRender;
+
+	/**
+	 * Formats the specified response.
+	 *
+	 * @param Response $response the response to be formatted.
+	 */
+	public function format($response)
+	{
+		$response->getHeaders()->set('Content-Type', 'application/pdf');
+		$response->content = $this->formatPdf($response);
+	}
+
+	/**
+	 * Formats response Latex in PDF
+	 *
+	 * @param Response $response
+	 */
+	protected function formatPdf($response)
+	{
+		\Yii::trace($response->data);
+
+
+	}
+}
